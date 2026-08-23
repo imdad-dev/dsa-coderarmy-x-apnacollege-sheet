d1345d094d0e9b7e78b1adeff46241297cd2286e
unknown <imdadulhoquekk@gmail.com>
2026-08-23 23:28:43 +0530

    lec 20 : bubble sort

---
+++ Lectures/Lecture_020/Homework/README.md
@@ -0,0 +1,7 @@
+## Homework Questions: 
+**Q1:** : Use Bubble Sort Algorithm to sort the array of integers in decreasing order. [Here](./Q1.cpp)
+
+**Q2:** Bubble Sort Algorithm to sort the array of integers in increasing order by taking the smallest number to first place by starting from the last. [Here](./Q2.cpp)
+
+
+**Q3:** Bubble Sort Algorithm to sort the array of char in ascending order. [Here](./Q3.cpp)
---
+++ Lectures/Lecture_020/Lecture_Code/sort.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    void bubbleSort(vector<int>& arr) {
+        // code here
+        int n =arr.size();
+         for(int i=0; i<n-1; i++){
+        
+        bool swap = false;
+        for(int j=0; j<n-i-1; j++){
+            
+            if(arr[j] > arr[j+1]){
+                int temp =arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                swap =true;
+            }
+            
+        }
+          if(!swap) break;  // already sorted break loop
+    }
+    
+    }
+};
+
+/*
+T.c = O(n^2)
+best case : omega(n)
+Avg : Theta(n^2)
+space complexity = O(1)
+*/
+
+
